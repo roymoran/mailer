8eeceb10a85ebaf6ed3c85f99cf3e3ba10657780
Roy Moran <roy_moran1993@yahoo.com>
2020-07-26 00:05:56 -0500

    remove unused ci vars (#16)
    * remove unused ci vars
    
    * migrate test app to net core v3.1

--- Mailer/Startup.cs
+++ Mailer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -32,7 +33,7 @@ namespace Mailer
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
         }
 
